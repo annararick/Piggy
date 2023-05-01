4cf6cc139ae9b12aa471ed94541ec990c4982934
annararick <125601408+annararick@users.noreply.github.com>
2023-05-01 18:46:56 +0000

    40

--- student.py
+++ student.py
@@ -71,7 +71,7 @@ class Piggy(PiggyParent):
   
     def dance(self):
         # lower-ordered example...
-      for x in range(30):
+      for x in range(40):
         self.right()
         time.sleep(.3)
         self.left()
